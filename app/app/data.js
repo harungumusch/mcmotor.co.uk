1d806218a46618be8ea63c5cf83db6fb4a279a91
Harun Gumus <harungumus.ch@gmail.com>
2022-02-06 20:36:29 +0100

    buy car used page has been done

---
+++ app/app/data.js
@@ -0,0 +1,31 @@
+// İlan No:
+// 19368252
+// İlan Tarihi:
+// 06 Şubat 2022
+// Marka:
+// Mercedes - Benz
+// Seri:
+// C
+// Model:
+// C 200 AMG
+// Yıl:
+// 2020
+// Yakıt Tipi:
+// Benzin
+// Vites Tipi:
+// Otomatik
+// Motor Hacmi:
+// 1497 cc
+// Motor Gücü:
+// 184 hp
+// Kilometre:
+// 12.000 km
+// Boya-değişen:
+// Belirtilmemiş
+// Takasa Uygun:
+// Takasa Uygun
+// Kimden:
+// Galeriden
+
+// ilan basligi:
+// aciklama:
